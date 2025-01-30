bfaf4cbbe8cd5dea3b0d80abec63d32e33e643d1
thomas-nurse <thomas.p.nurse@gmail.com>
2025-01-30 16:04:00 +0000

    initial commit

---
+++ src/main/java/com/impillagers/mod/Impillagers.java
@@ -0,0 +1,17 @@
+package com.impillagers.mod;
+
+import com.impillagers.mod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Impillagers implements ModInitializer {
+	public static final String MOD_ID = "impillagers";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+	}
+}
