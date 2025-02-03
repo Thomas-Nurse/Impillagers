475662f3e9a959c865cedfe400e3ea380b4b2c87
thomas-nurse <thomas.p.nurse@gmail.com>
2025-02-03 13:34:26 +0000

    Implemented the First Custom Trade for Impillagers

--- src/main/java/com/impillagers/mod/Impillagers.java
+++ src/main/java/com/impillagers/mod/Impillagers.java
@@ -9,6 +9,11 @@ import com.impillagers.mod.villager.DungCollector;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,5 +32,13 @@ public class Impillagers implements ModInitializer {
 
 		FabricDefaultAttributeRegistry.register(ModEntities.IMPILLAGER, ImpillagerEntity.createVillagerAttributes());
 
+
+		//Trades
+
+		TradeOfferHelper.registerVillagerOffers(Banker.BANKER, 1, factories -> {
+			factories.add((entity, random) -> new TradeOffer(
+					new TradedItem(Items.EMERALD, 3),
+					new ItemStack(ModItems.DUNG_BALL, 8), 7, 2, 0.04f));
+	});
 	}
 }
